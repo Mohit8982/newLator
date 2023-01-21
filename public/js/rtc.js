b25f611a6ad6f5e41f5b3240eac2161262e0c4e3
MOHIT BARUA <baruamohit57@gmail.com>
2023-01-21 14:20:23 +0530

    Update rtc.js

--- public/js/rtc.js
+++ public/js/rtc.js
@@ -90,8 +90,8 @@ window.addEventListener( 'load', () => {
 
             function say(m) {
                 var msg = new SpeechSynthesisUtterance();
-                var voices = window.speechSynthesis.getVoices();
-                msg.voice = voices[10];
+             //   var voices = window.speechSynthesis.getVoices();
+             //   msg.voice = voices[10];
                 msg.voiceURI = "native";
                 msg.volume = 1;
                 msg.rate = 1;
